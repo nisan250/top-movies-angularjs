156f58c4f00e09b1210fc582bf62f85774ef2cfa
nisan250 <nisan250@gmail.com>
2018-12-20 07:52:34 +0000

    11.Array management filters

--- docs/js/app.js
+++ docs/js/app.js
@@ -3,5 +3,6 @@ var myApp =angular.module('myApp', []);
 myApp.controller('AppController', function AppController($scope, $http) {
     $http.get('js/data.json').then(function(response) {
         $scope.movies = response.data;
+        $scope.movieOrder = 'name';
     });
 });
